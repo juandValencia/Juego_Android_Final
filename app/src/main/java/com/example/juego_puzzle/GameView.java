8b7f8a0c779fb4dd6f423bd888dcd8f589be4f41
SantigoRes <santiago.restrepov@upb.edu.co>
2021-05-06 13:19:13 -0500

    clases pricnipales para la vista

---
+++ app/src/main/java/com/example/juego_puzzle/GameView.java
@@ -0,0 +1,12 @@
+package com.example.juego_puzzle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView  extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
