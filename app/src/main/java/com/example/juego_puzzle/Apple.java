e638301a9504c2e2ce1114732128fef25bdb76cb
SantigoRes <santiago.restrepov@upb.edu.co>
2021-05-07 12:30:20 -0500

    Constructor manzanas

---
+++ app/src/main/java/com/example/juego_puzzle/Apple.java
@@ -0,0 +1,58 @@
+package com.example.juego_puzzle;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+public class Apple {
+    private Bitmap bm;
+    private int x, y;
+    private Rect r;
+
+    public Apple(Bitmap bm, int x, int y) {
+        this.bm = bm;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(bm, x, y, null);
+    }
+
+    public void reset(int nx, int ny){
+        this.x = nx;
+        this.y = ny;
+    }
+
+    public Bitmap getBm() {
+        return bm;
+    }
+
+    public void setBm(Bitmap bm) {
+        this.bm = bm;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Rect getR() {
+        return new Rect(this.x, this.y, this.x+GameView.sizeElementMap, this.y+GameView.sizeElementMap);
+    }
+
+    public void setR(Rect r) {
+        this.r = r;
+    }
+}
